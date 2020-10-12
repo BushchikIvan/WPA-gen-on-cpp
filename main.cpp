022fc868b88fbc7b6d2763ac44a7584ee51ee90a
ivabus <ivabus06@yandex.ru>
2020-10-12 16:55:42 +0300

    Changing full programm

--- main.cpp
+++ main.cpp
@@ -1,46 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <clocale>
+//#define NULL __null
 using namespace std;
 
-int main() {
-    ofstream outf("list.txt");
-    string temp;
-    const string mass[159] = {"", " ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "а", "А", "б", "Б", "в", "В", "г", "Г", "д", "Д", "е", "Е", "ё", "Ё", "ж", "Ж", "з", "З", "и", "И", "й", "Й", "к", "К", "л", "Л", "м", "М", "н", "Н", "о", "О", "п", "П", "р", "Р", "с", "С", "т", "Т", "у", "У", "ф", "Ф", "х", "Х", "ц", "Ц", "ч", "Ч", "ш", "Щ", "ъ", "Ъ", "ы", "Ы", "ь", "Ь", "э", "Э", "ю", "Ю", "я", "Я", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", ",", ".", "?", "/"};
-    for(int a = 0; a < 159; a++) {
-        for (int b = 0; b < 159; b++) {
-            for (int c = 0; c < 159; c++) {
-                for (int d = 0; d < 159; d++) {
-                    for (int e = 0; e < 159; e++) {
-                        for (int f = 0; f < 159; f++) {
-                            for (int g = 0; g < 159; g++) {
-                                for (int h = 0; h < 159; h++) {
-                                    for (int i = 1; i < 159; i++) {
-                                        for (int j = 1; j < 159; j++) {
-                                            for (int k = 1; k < 159; k++) {
-                                                for (int l = 1; l < 159; l++) {
-                                                    for (int m = 1; m < 159; m++) {
-                                                        for (int n = 1; n < 159; n++) {
-                                                            for (int o = 1; o < 159; o++) {
-                                                                for (int p = 1; p < 159; p++) {
-                                                                    temp = mass[a] + mass[b] + mass[c] + mass[d] + mass[e] + mass[f] + mass[g] + mass[h] + mass[i] + mass[j] + mass[k] + mass[l] + mass[m] + mass[n] + mass[o] + mass[p];
-                                                                    outf << temp << endl;
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+const string mass[91] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", ",", ".", "?", "/"};
+ofstream outf("list.txt");
+void gen (string pass, int ssize, int len)
+{
+    if (len == 0)
+    {
+        outf << pass << endl;
+        return;
     }
+
+    pass += mass[0];
+    gen(pass, ssize + 1, len - 1);
+
+    for (int i = 1; i < 91; ++i)
+    {
+        pass[ssize] = mass[i][0];
+        gen(pass, ssize + 1, len - 1);
+    }
+}
+
+int main()
+{
+    setlocale(0,"");
+    string s = "";
+    gen(s, 0, 8);
 
     return 0;
 }
